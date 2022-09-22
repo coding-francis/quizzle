8147918a83ec7316d2d398710a1632da00a908ec
codingdrizzle <codingfrancis100@gmail.com>
2022-09-22 21:51:28 +0000

    been a while

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,7 @@ const LoginPage = () => {
   return (
     <div>
         <h1 style={{textAlign: 'center'}}>Quizzles</h1>
+        <h1 style={{textAlign: 'center'}}>Login Page</h1>
     </div>
   )
 }
